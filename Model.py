7abd6b06945eaf0c3c25bf40aa6157d4612047bd
SCZ-MSI <shaoshixin842@live.com>
2018-06-25 22:45:44 +0800

    Frame.__init__(self, master)

--- Model.py
+++ Model.py
@@ -2,37 +2,40 @@ from tkinter import *
 from tkinter.messagebox import *
 
 
-class Model:
+class Model(Frame):
+
     def __init__(self, master=None):
+        Frame.__init__(self, master)
         self.root = master
         self.username = StringVar()
         self.password = StringVar()
         self.creatPage()
 
     def creatPage(self):
-        self.frame = Frame(self.root)
-        self.frame.pack()
-        # self.frame = Frame(self.root)  # 创建Frame
-        Label(self.frame).grid(row=0, stick=W)
-        Label(self.frame, text='账户: ').grid(row=1, stick=W, pady=10)
-        Entry(self.frame, textvariable=self.username).grid(row=1, column=1,
-                                                           stick=E)
-        Label(self.frame, text='密码: ').grid(row=2, stick=W, pady=10)
-        Entry(self.frame, textvariable=self.password, show='*').grid(row=2,
-                                                                     column=1,
-                                                                     stick=E)
-        Button(self.frame, text='登陆', command=self.loginCheck).grid(row=3,
-                                                                    stick=W,
-                                                                    pady=10)
-        Button(self.frame, text='退出', command=self.frame.quit).grid(row=3,
-                                                                    column=1,
-                                                                    stick=E)
+        Label(self, text='账户: ').pack()
+        Entry(self, textvariable=self.username).pack()
+        Label(self, text='密码: ').pack()
+        Entry(self, textvariable=self.password, show='*').pack()
+        Button(self, text='登陆', command=self.loginCheck).pack()
+        Button(self, text='退出', command=self.root.quit).pack()
 
     def loginCheck(self):
         name = self.username.get()
         secret = self.password.get()
         if name == '1' and secret == '1':
-            self.frame.destroy()
+            self.root.destroy()
             showinfo(title='成功', message='cheng')
         else:
             showinfo(title='错误', message='账号或密码错误！')
+
+
+if __name__ == '__main__':
+    root = Tk()
+    root.title('nihao')
+    main_page = Model(root)
+    width = 600
+    height = 600
+    root.geometry(
+        '%dx%d+%d+%d' % (width, height, (root.winfo_screenwidth() - width) / 2,
+                         (root.winfo_screenheight() - height) / 2))
+    root.mainloop()
--- main.py
+++ main.py
@@ -7,50 +7,63 @@
 
 
 from tkinter import *
-import Model as ma
-
-
-
+from Model import *
+from frame import *
+from f1 import *
 
 
 class MainPage:
     def __init__(self, master=None):
         self.root = master
-        self.creatPage()
+        # self.main_menu_frm = Frame(self.root)
+        self.model_frm = Model(self.root)
+        self.model_frm.pack()
+        self.help_frm = Fr(self.root)
+        self.creatMenu()
 
     def __quit(self):
         self.root.quit()
         root.destroy()
         exit()
 
-    def creatPage(self):
-
-        m = ma(self.root)
+    def creatMenu(self):
 
         lab = Label(self.root, text='测试')
         lab.pack();
+        # self.main_menu_frm.pack()
+
         menuBar = Menu(self.root)
-        fileMenu = Menu(menuBar, tearoff=False)
-        fileMenu.add_command(label='账户密码组合生成！', command=go)
+        fileMenu = Menu(menuBar, tearoff=0)
+        fileMenu.add_command(label='账户密码组合生成！', command=self.go)
         menuBar.add_cascade(label='数据生成', menu=fileMenu)
         menuBar.add_separator()
         urlMenu = Menu(menuBar, tearoff=False)
-        urlMenu.add_command(label='占位', command=m())
-        urlMenu.add_command(label='占位', command=go)
+        urlMenu.add_command(label='占位1', command=self.go)
+        urlMenu.add_command(label='占位2', command=self.go1)
+        urlMenu.add_command(label='占位3', command=self.go2)
         menuBar.add_cascade(label='url输入', menu=urlMenu)
         menuBar.add_separator()
-        menuBar.add_command(label='退出', command=self.__quit)
+        menuBar.add_radiobutton(label='退出', command=self.__quit)
         root.config(menu=menuBar)
 
+    def go(self):
+        self.model_frm.forget()
+        self.help_frm.forget()
 
-def go():
-    print("go")
+    def go1(self):
+        self.model_frm.forget()
+        self.help_frm.pack()
+
+    def go2(self):
+        self.model_frm.pack()
+        self.help_frm.forget()
+
 
 
 if __name__ == '__main__':
     root = Tk()
     root.title('nihao')
-    main_page = MainPage(root)
+    MainPage(root)
     width = 600
     height = 600
     root.geometry(
